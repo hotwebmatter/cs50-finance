38f8ee12294f808495966945cb8e342c96b8032a
Matt Obert <matt.obert@gmail.com>
2016-07-30 02:18:46 +0000

    Working on register view and controller.

--- public/register.php
+++ public/register.php
@@ -13,10 +13,65 @@
     // else if user reached page via POST (as by submitting a form via POST)
     else if ($_SERVER["REQUEST_METHOD"] == "POST")
     {
-        if (empty($_POST["username"]) || empty($_POST["password"])
+        // validate submission
+        if (empty($_POST["username"]))
         {
-            apologize("Please provide a valid username and password.");
+            apologize("You must provide your username.");
         }
+        else if (empty($_POST["password"]) || empty($_POST["confirmation"]))
+        {
+            apologize("You must provide your password.");
+        }
+        else if ($_POST["password"] != $_POST["confirmation"])
+        {
+            apologize("Passwords do not match!");
+        }
+
+        // query database for user
+        $rows = CS50::query("SELECT * FROM users WHERE username = ?", $_POST["username"]);
+
+        // if we found user, do not register
+        if (count($rows) == 1)
+        {
+            apologize("Username is already taken.");
+        }
+        
+        // otherwise, register user
+        $result = CS50::query("INSERT IGNORE INTO users (username, hash, cash) VALUES(?, ?, 10000.0000)", $_POST["username"], password_hash($_POST["password"], PASSWORD_DEFAULT));
+        if ($result === false)
+        {
+            apologize("ERROR: Could not register new user.");
+        }
+
+        $rows = CS50::query("SELECT LAST_INSERT_ID() AS id");
+          $id = $rows[0]["id"];
+
+        // remember that user's now logged in by storing user's ID in session
+        $_SESSION["id"] = $row["id"];
+  
+        // redirect to portfolio
+        redirect("/");
+
+/*      // check password
+ *      if (count($rows) == 1)
+ *      {
+ *          // first (and only) row
+ *          $row = $rows[0];
+ *
+ *          // compare hash of user's input against hash that's in database
+ *          if (password_verify($_POST["password"], $row["hash"]))
+ *          {
+ *              // remember that user's now logged in by storing user's ID in session
+ *              $_SESSION["id"] = $row["id"];
+ *
+ *              // redirect to portfolio
+ *              redirect("/");
+ *          }
+ *      }
+ */
+ 
+        // else apologize
+        apologize("Invalid username and/or password.");
     }
 
 ?>
--- views/register_form.php
+++ views/register_form.php
@@ -7,12 +7,12 @@
             <input class="form-control" name="password" placeholder="Password" type="password"/>
         </div>
         <div class="form-group">
-            <input class="form-control" name="confirmation" placeholder="Confirm" type="password"/>
+            <input class="form-control" name="confirmation" placeholder="Confirm Password" type="password"/>
         </div>
         <div class="form-group">
             <button class="btn btn-default" type="submit">
                 <span aria-hidden="true" class="glyphicon glyphicon-log-in"></span>
-                Log In
+                Register
             </button>
         </div>
     </fieldset>
