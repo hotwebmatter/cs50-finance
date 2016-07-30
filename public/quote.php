676f5acb74b56aa7b82fe3992089bed6c4b2ec49
Matt Obert <matt.obert@gmail.com>
2016-07-30 03:22:04 +0000

    Started work on quote_form view and quote controller

---
+++ public/quote.php
@@ -0,0 +1,30 @@
+<?php
+
+    // configuration
+    require("../includes/config.php"); 
+
+    // if user reached page via GET (as by clicking a link or via redirect)
+    if ($_SERVER["REQUEST_METHOD"] == "GET")
+    {
+        // else render form
+        render("quote_form.php", ["title" => "Get Quote"]);
+    }
+
+    // else if user reached page via POST (as by submitting a form via POST)
+    else if ($_SERVER["REQUEST_METHOD"] == "POST")
+    {
+        // validate submission
+        if (empty($_POST["symbol"]))
+        {
+            apologize("You must provide a symbol.");
+        }
+
+        // Look up that stock
+        $s = lookup($_POST["symbol"]);
+
+        //  ???
+        // Profit
+
+    }
+
+?>
