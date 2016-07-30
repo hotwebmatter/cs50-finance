35f07fb533ab80fc34a6e5cdeefa10462b7c0d74
Matt Obert <matt.obert@gmail.com>
2016-07-30 20:59:08 +0000

    Completed portfolio model, view, controller and CSS. Rewrote quote view to make it more "templatey".

--- public/index.php
+++ public/index.php
@@ -3,7 +3,27 @@
     // configuration
     require("../includes/config.php"); 
 
+    // look up user's stock portfolio
+    $rows = CS50::query("SELECT * FROM portfolios WHERE user_id = ?", $_SESSION["id"]);
+    
+    // if 
+    $positions = [];
+    foreach ($rows as $row)
+    {
+        $stock = lookup($row["symbol"]);
+        if ($stock !== false)
+        {
+            $positions[] = [
+                "symbol" => $row["symbol"],
+                "name" => $stock["name"],
+                "shares" => $row["shares"],
+                "price" => $stock["price"],
+                "total" => $row["shares"] * $stock["price"]
+            ];
+        }
+    }
+
     // render portfolio
-    render("portfolio.php", ["title" => "Portfolio"]);
+    render("portfolio.php", ["positions" => $positions, "title" => "Portfolio"]);
 
 ?>
--- views/quote.php
+++ views/quote.php
@@ -1,4 +1,8 @@
 <?php
     // Profit
-    print("A share of " . $name . " (" . $symbol . ") costs <b>\$" . number_format($price, 2) . "</b>." );
+    // print("A share of " . $name . " (" . $symbol . ") costs <b>\$" . number_format($price, 2) . "</b>." );
 ?>
+
+<p>
+    A share of <?= $name ?> (<?= $symbol ?>) costs <b>$<?= $price ?></b>.
+</p>
